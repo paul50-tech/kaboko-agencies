1b72d34557ac879df1cece2aa153a54b3d938f21
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-22 10:11:02 +0300

    add server.json

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
